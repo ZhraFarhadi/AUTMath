859403521008b14f427b2c2fae9c93862e387802
Zahra <zhra.farhadii@gmail.com>
2026-06-04 15:22:47 +0330

    multiply

--- src/AUTMath.java
+++ src/AUTMath.java
@@ -33,7 +33,7 @@ public class AUTMath {
      */
     public static int multiply(int num1, int num2) {
         int result = 0;
-        for (int i = 0; i <= num2; i++) {
+        for (int i = 0; i < num2; i++) {
            result += num1;
         }
         return result;
